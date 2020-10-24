aec5145720ae421a81d79f7f4809099032848946
Marcin Bialek <mbialus@gmail.com>
2020-10-25 00:58:57 +0200

    DebugExample

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myNick = "Bialy";
+        String myColor = "Niebieski";
+        int myNumer = 23;
+        int myWeight = 74;
+        double mySecondnumber = 23.5;
+        char myletter = 'Z';
+        System.out.println(myNick);
+        System.out.println(myColor);
+        System.out.println(myNumer);
+        System.out.println(myWeight);
+        System.out.println(mySecondnumber);
+        System.out.println(myletter);
+    }
+}
