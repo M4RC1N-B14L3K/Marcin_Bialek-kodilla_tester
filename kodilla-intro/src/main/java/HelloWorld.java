7881988bbbcba8abcb6b45b7d4f92072183a7cfa
Marcin Bialek <mbialus@gmail.com>
2020-10-24 23:39:41 +0200

    HelloWorld

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println();
+        String myName = "Marcin";
+        int myAge = 30;
+        char mySurname = 'B';
+    }
+}
