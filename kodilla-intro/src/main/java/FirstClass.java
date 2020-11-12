178f3fb7fd63133329e57d3399d90bad2fbb5f6f
Marcin Bialek <mbialus@gmail.com>
2020-11-12 18:32:07 +0100

    Notebook

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,5 +1,16 @@
-public class FirstClass {                           // [1]
-    public static void main(String[] args) {         // [2]
-        System.out.println("Hello from FirstClass!"); // [3]
+public class FirstClass {
+    public static void main(String[] args) {
+
+        Notebook notebook = new Notebook("600g", 1000);
+        System.out.println(notebook.weight + "" + notebook.price);
+        notebook.checkPrice();
+
+        Notebook heavyNotebook = new Notebook("2000g", 1500);
+        System.out.println(heavyNotebook.weight + "" + notebook.price);
+        notebook.checkPrice();
+
+        Notebook oldNotebook = new Notebook("1600g", 500);
+        System.out.println(oldNotebook.weight + "" + notebook.price);
+        notebook.checkPrice();
     }
 }
