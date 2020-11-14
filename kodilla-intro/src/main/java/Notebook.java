86dde9dbe5e6e064a0e96189c57998a67e92bdcb
Marcin Bialek <mbialus@gmail.com>
2020-11-14 15:27:21 +0100

    Notebook

--- kodilla-intro/src/main/java/Notebook.java
+++ kodilla-intro/src/main/java/Notebook.java
@@ -1,9 +1,9 @@
 public class Notebook {
-    String weight;
+    int weight;
     int price;
     int year;
 
-    public Notebook(String weight, int price, int year) {
+    public Notebook(int weight, int price, int year) {
         this.weight = weight;
         this.price = price;
         this.year = year;
@@ -20,9 +20,9 @@ public class Notebook {
     }
 
     public void checkWeight() {
-        if (this.weight == "600g") {
+        if (this.weight < 1000) {
             System.out.println("This notebook is light weight.");
-        } else if (this.weight == "1600g") {
+        } else if (this.weight > 1000 && this.weight < 2000) {
             System.out.println("This notebook is not too heavy.");
         } else {
             System.out.println("This notebook is heavy.");
@@ -30,6 +30,12 @@ public class Notebook {
     }
 
     public void checkYear() {
-        if (this.year )
+        if (this.year < 2018) {
+            System.out.println("This notebook is too old.");
+        } else if (this.year < 2020) {
+            System.out.println("This notebook is quite new.");
+        } else {
+            System.out.println("This notebook is brand new.");
+        }
     }
 }
