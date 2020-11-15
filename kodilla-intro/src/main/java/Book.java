a1d9e8a831bc00d14af3988a6d5cfb07488737ba
Marcin Bialek <mbialus@gmail.com>
2020-11-15 21:22:56 +0100

    Book

--- kodilla-intro/src/main/java/Book.java
+++ kodilla-intro/src/main/java/Book.java
@@ -3,8 +3,8 @@ public class Book {
     private String author;
     private String title;
 
-    public static String of(String author, String title) {
-        Book book = new Book("Isaac Asimov", "The Galaxy");
-        return book;
+    public String (String author, String title) {
+        Book book = Book.of("Isaac Asimov", "The Galaxy");
+        return Book;
     }
 }
